81f5a7abe4156c1afeee58b6ea8811cb9d63ddb2
Akimeo <akim.parinoff@gmail.com>
2021-08-27 16:39:08 +0300

    File attachment for edited resources added

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 module Qna
   class Application < Rails::Application
-    VERSION = '0.4.0'.freeze
+    VERSION = '0.5.0'.freeze
   end
 end
--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -39,6 +39,16 @@ feature 'Author can edit answer', %q{
         expect(page).to have_selector 'textarea'
       end
     end
+
+    scenario 'edits their answer with attaching files' do
+      within '.answers' do
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario "Authenticated user tries to edit someone else's answer" do
